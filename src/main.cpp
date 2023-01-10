9c3440f17aa25a44bf47ce7653cfa6f05e702140
piwer-m <97893788+piwer-m@users.noreply.github.com>
2023-01-10 18:19:54 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,26 @@
+#include "is.h"
 
+int main() {
+	setlocale(LC_ALL, "ru");
+	int mode; std::cin >> mode;
+
+	if (mode == 1) {
+		std::ifstream code;
+		code.open("main.is");
+		std::ofstream output;
+		output.open("compiled.cpp");
+		if ((code.is_open()) && (output.is_open())) {
+			std::string s = "";
+			output << "#include \"is.h\"";
+			output << "\n\n\nint execute_is() {";
+			while (getline(code, s)) {
+				output << "\n\t" << s;
+			}
+			output << "\n}";
+		}
+	}
+	else {
+		execute_is();
+		system("pause");
+	}
+}
